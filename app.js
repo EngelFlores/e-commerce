e4b447645d417eccd3ea0460394e3e593f77c10f
AndreiRupertti <arupertti@gmail.com>
2018-01-10 19:22:54 -0200

    <@AndreiRupertti> altera porta do servidor

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require(`express`);
 const sendString = require(`./sendString`)
 const app = express()
 
+app.set('port', (process.env.PORT || 3000))
+
 app.get('/', (req,res) => {
   res.send(sendString())
 })
 
-app.listen(3000)
+app.listen(app.get('port'), () =>
+  console.log(`Node app is running on port ${app.get('port')}`))
