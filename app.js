a3865787794b07eaa076db537ef14059c679337f
Matheus <matheus.mvo10@gmail.com>
2018-01-11 20:12:30 -0200

    mocka dados

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const app = express()
 app.set('port', (process.env.PORT || 3000))
 
 app.get('/', (req,res) => {
-  res.send(sendString())
+  const products = [{name: "Vassoura",
+              price: "10",
+              quantity: "5"},
+              {name: "Abajur",
+              price: "15",
+              quantity: "1"}]
+  res.send(products)
 })
 
 app.listen(app.get('port'), () =>
-  console.log(`Node app is running on port ${app.get('port')}`))
+console.log(`Node app is running on port ${app.get('port')}`))
