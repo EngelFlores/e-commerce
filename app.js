69195824c308c10b438e407a7ef5b4a134ad9f1c
Matheus <matheus.mvo10@gmail.com>
2018-01-11 21:01:12 -0200

    Configura handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require(`express`);
-const sendString = require(`./sendString`)
+const exphbs  = require('express-handlebars');
+const sendString = require(`./sendString`);
 const app = express()
+
+app.use(express.static(__dirname + '/views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.set('port', (process.env.PORT || 3000))
 
-app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req,res) => {
   const products = [{name: "Vassoura",
@@ -13,7 +17,11 @@ app.get('/', (req,res) => {
               {name: "Abajur",
               price: "15",
               quantity: "1"}]
+
+
+res.render('index')
 })
+
 
 app.listen(app.get('port'), () =>
 console.log(`Node app is running on port ${app.get('port')}`))
