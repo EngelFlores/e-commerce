38638aa0105766cda518b1418bdcc5b586ea412d
Matheus <matheus.mvo10@gmail.com>
2018-01-12 01:06:06 -0200

    adiciona teste de verificação de disponibilidade

--- test/verifyDisponibility_test.js
+++ test/verifyDisponibility_test.js
@@ -2,31 +2,48 @@ const chai = require('chai')
 const expect = chai.expect
 const verifyDisponibility = require('../utilities/verifyDisponibility')
 
-const mockedData = [{name: "Vassoura",
-                    price: "10",
-                    quantity: "0"},
-                    {name: "Abajur",
-                    price: "15",
-                    quantity: "1"}]
 
 describe('verifyDisponibility', () => {
   context('com quantity igual 0', () => {
     it('Retorna array alterado', () => {
+      const mockedData = [{name: "Vassoura",
+      price: "10",
+      quantity: "0"},
+      {name: "Abajur",
+      price: "15",
+      quantity: "1"}]
       const result = verifyDisponibility(mockedData)
       expect(result).to.be.eql([
-      {
-      name: "Vassoura",
+        {
+          name: "Vassoura",
+          price: "10",
+          quantity: "indisponivel"
+        },
+        {
+          name: "Abajur",
+          price: "15",
+          quantity: "1"
+        }
+      ])
+    })
+  })
+  context('com quantity maior que 0', () => {
+    it('Retorna array inaltarado', () => {
+      const mockedData = [{name: "Vassoura",
       price: "10",
-      quantity: "indisponivel"
-      },
-      {
-      name: "Abajur",
+      quantity: "4"},
+      {name: "Abajur",
       price: "15",
-      quantity: "1"
-      }
-    ])
+      quantity: "1"}]
+      const result = verifyDisponibility(mockedData)
+      expect(result).to.be.eql([
+        {name: "Vassoura",
+        price: "10",
+        quantity: "4"},
+        {name: "Abajur",
+        price: "15",
+        quantity: "1"}
+      ])
+    })
   })
-
-  })
-  
 })
